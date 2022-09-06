a46615e2d9204d0f09588a9478cd9d33a1e1e0d9
minho <joo25sk@naver.com>
2022-09-06 20:48:11 +0900

    1. 프로젝트 생성

---
+++ src/main/java/com/narsm/web/MyGoalApplication.java
@@ -0,0 +1,13 @@
+package com.narsm.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGoalApplication.class, args);
+	}
+
+}
