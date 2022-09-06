20bb013b4688ae11c7b24b95735dbcfc663b8319
minho <joo25sk@naver.com>
2022-09-07 04:58:31 +0900

    28. 관심 주제 뷰 구현

--- src/main/java/com/narsm/web/module/account/domain/entity/Tag.java
+++ src/main/java/com/narsm/web/module/account/domain/entity/Tag.java
@@ -1,13 +1,14 @@
 package com.narsm.web.module.account.domain.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,5 +18,6 @@ public class Tag {
     @Id
     @GeneratedValue
     private Long id;
+    @Column(unique = true, nullable = false)
     private String title;
 }
