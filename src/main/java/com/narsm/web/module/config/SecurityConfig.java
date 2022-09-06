b5c20e74332fa0fce3082660d85c0ff6a65b2587
minho <joo25sk@naver.com>
2022-09-07 01:51:22 +0900

    16. 로그인 유지(RememberMe) 기능 구현

--- src/main/java/com/narsm/web/module/config/SecurityConfig.java
+++ src/main/java/com/narsm/web/module/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.narsm.web.module.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,13 +9,21 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import com.narsm.web.module.account.application.AccountService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+	private final AccountService accountService;
+    private final DataSource dataSource;
+	
 	@Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -26,18 +36,23 @@ public class SecurityConfig {
         	.permitAll();
         http.logout()
         	.logoutSuccessUrl("/");
+        http.rememberMe()
+        	.userDetailsService(accountService)
+        	.tokenRepository(tokenRepository());
         return http.build();
     }
-
+	
+	@Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+	
     @Bean
     protected WebSecurityCustomizer webSecurityCustomizer() throws Exception {
         return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
         		.mvcMatchers("/node_modules/**", "/images/**")
         		.antMatchers("/h2-console/**");
-    }
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
