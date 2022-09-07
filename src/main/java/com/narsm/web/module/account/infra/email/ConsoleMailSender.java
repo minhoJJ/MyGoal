44185f7bbefe87921abccdb44aaa4c334a4413bd
minho <joo25sk@naver.com>
2022-09-08 02:20:41 +0900

    35. SMTP 설정(feat. Gmail)

--- src/main/java/com/narsm/web/module/account/infra/email/ConsoleMailSender.java
+++ src/main/java/com/narsm/web/module/account/infra/email/ConsoleMailSender.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Profile({"local", "local-db"})
+@Profile("local")
 @Component
 @Slf4j
 public class ConsoleMailSender implements JavaMailSender {
