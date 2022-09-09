62875a7e06e70bfdb59e113b8c7224d4fab2ac6a
minho <joo25sk@naver.com>
2022-09-10 01:50:55 +0900

    71. 에러 핸들러 및 뷰 구현

---
+++ src/main/java/com/narsm/web/app/modules/main/endpoint/controller/ExceptionAdvice.java
@@ -0,0 +1,33 @@
+package com.narsm.web.app.modules.main.endpoint.controller;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.narsm.web.app.modules.account.domain.entity.Account;
+import com.narsm.web.app.modules.account.support.CurrentUser;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionAdvice {
+
+  @ExceptionHandler()
+  public String handleRuntimeException(@CurrentUser Account account, HttpServletRequest request,
+      RuntimeException exception) {
+    log.info(getNicknameIfExists(account) + "requested {}", request.getRequestURI());
+    log.error("bad request", exception);
+    return "error";
+  }
+
+  private String getNicknameIfExists(Account account) {
+    return Optional.ofNullable(account)
+        .map(Account::getNickname)
+        .map(s -> s + " ")
+        .orElse("");
+  }
+}
