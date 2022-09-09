fada71fc027af13e93d498b27859a5fcf9c94be5
minho <joo25sk@naver.com>
2022-09-09 23:39:51 +0900

    59. 알림 도메인 설계

---
+++ src/main/java/com/narsm/web/app/modules/notification/domain/entity/Notification.java
@@ -0,0 +1,40 @@
+package com.narsm.web.app.modules.notification.domain.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.narsm.web.app.modules.account.domain.entity.Account;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private boolean checked;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime created;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+}
---
+++ src/main/java/com/narsm/web/app/modules/notification/domain/entity/NotificationType.java
@@ -0,0 +1,7 @@
+package com.narsm.web.app.modules.notification.domain.entity;
+
+public enum NotificationType {
+    STUDY_CREATED,
+    STUDY_UPDATED,
+    EVENT_ENROLLMENT,
+}
